e264112287294c3476ad541361390b6155be7d0d
sergJess <serg.fantasy@yandex.ru>
2022-05-12 08:43:38 +0300

    feat: add base config

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+
+		plugins: [new HtmlWebpackPlugin({
+			path: path.resolve(__dirname, './src'),
+		})],
+
+		devServer: {
+			static: {
+					directory: path.join(__dirname, 'src'),
+			},
+			allowedHosts: 'auto',
+			compress: true,
+			port: 9000,
+			client: {
+				overlay: true,
+		},
+		open: true,
+	},
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+		
+};
