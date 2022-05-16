00b9b47d55d7d83cea3ab2381cd1b76e513c3a28
sergJess <serg.fantasy@yandex.ru>
2022-05-16 08:25:04 +0300

    feat: add render start page

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ import {NodeCreator} from './helpers/node-creator';
 import StartPage from './start-page/start-page'; './start-page/start-page';
 const root = new NodeCreator(document.body, document.createElement('div'), {id:	'root'}).getElement();
 const startPage = new StartPage(root, document.createElement('div'), {classList: ['start-page']});
+startPage.render();
 
--- src/start-page/start-page.ts
+++ src/start-page/start-page.ts
@@ -4,5 +4,10 @@ export default class StartPage extends NodeCreator{
 constructor(parent: HTMLElement, element: HTMLElement, nodeParams: nodeParametrs){
 	super(parent, element, nodeParams);
 }
-
+render(): void {
+	const startButton = new NodeCreator(this.getElement(), document.createElement('button'), {
+		classList: ['button', 'start-page-button'],
+  text: 'Start'
+});
+}
 }
