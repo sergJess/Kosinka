8d434414206700aec3dabbc1c6806b8fc64c92b4
sergJess <serg.fantasy@yandex.ru>
2022-05-16 21:58:24 +0300

    feat: add styles for start page and body

--- src/start-page/start-page.ts
+++ src/start-page/start-page.ts
@@ -5,6 +5,10 @@ constructor(parent: HTMLElement, element: HTMLElement, nodeParams: nodeParametrs
 	super(parent, element, nodeParams);
 }
 render(): void {
+	const startText =  new NodeCreator(this.getElement(), document.createElement('h2'), {
+		classList: ['start-page-title'],
+  text: 'Game Kosinka'
+});
 	const startButton = new NodeCreator(this.getElement(), document.createElement('button'), {
 		classList: ['button', 'start-page-button'],
   text: 'Start'
